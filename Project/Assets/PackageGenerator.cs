45cdf01fe5b0bd0b66b42bd6586d0c1483cd20ab
jasonm-unity <jasonm@unity3d.com>
2017-01-31 14:21:35 -0500

    Added pre-Packaged package

---
+++ Project/Assets/PackageGenerator.cs
@@ -0,0 +1,35 @@
+//#if UNITY_EDITOR
+using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEditor;
+
+namespace Unity.Vfx.Cameras
+{
+
+	public class AlembicImporterPackaging
+	{
+		[MenuItem("Assets/VFx/Cameras/Make Package")]
+		public static void MakePackage()
+		{
+			string[] files = new string[]
+			{
+				"Assets/VFx-Camera/Scripts/PhysicalCamera.cs",
+
+				"Assets/VFx-Camera/Scripts/Model/MathematicalModel.cs",
+				"Assets/VFx-Camera/Scripts/Model/PhysicalCameraBodyModel.cs",
+				"Assets/VFx-Camera/Scripts/Model/PhysicalCameraLensModel.cs",
+				"Assets/VFx-Camera/Scripts/Model/PhysicalCameraModel.cs",
+				"Assets/VFx-Camera/Scripts/Model/StereoPhysicalCameraModel.cs",
+
+				"Assets/VFx-Camera/Scripts/Editor/PhysicalCameraEditor.cs",
+			};
+			AssetDatabase.ExportPackage(files, "VFX-PhysicalCameras.unitypackage", ExportPackageOptions.Recurse);
+		}
+	}
+}
+
+//#endif // UNITY_EDITOR
